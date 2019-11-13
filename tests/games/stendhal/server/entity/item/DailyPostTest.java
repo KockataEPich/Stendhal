d62289ee41754928009865c1d5de67cbd7ff89b1
Karl Meakin <karl.meakin@student.manchester.ac.uk>
2019-11-13 19:32:09 +0000

    Remove useless setup/teardown from DailyPostTest

--- tests/games/stendhal/server/entity/item/DailyPostTest.java
+++ tests/games/stendhal/server/entity/item/DailyPostTest.java
@@ -6,9 +6,6 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -25,18 +22,6 @@ public class DailyPostTest extends QuestHelper {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		ItemTestHelper.generateRPClasses();
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
 	}
 
 	@Test
