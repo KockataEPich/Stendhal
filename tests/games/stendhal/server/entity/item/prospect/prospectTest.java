b77f6d8348be2da96486dc5451f212e85b216c16
Bogdan Gabriel Tudor <bogdan-gabriel.tudor@student.manchester.ac.uk>
2019-11-14 11:31:17 +0000

    Added some comments to explain the prospectus item test

--- tests/games/stendhal/server/entity/item/prospect/prospectTest.java
+++ tests/games/stendhal/server/entity/item/prospect/prospectTest.java
@@ -10,9 +10,12 @@ import org.junit.Test;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.entity.item.Item;
 public class prospectTest {
+	// A test to verify the description of an item.
 	@Test
 	public void testDescribe() {
+		// Create a new item
 		final Item newprospectus = SingletonRepository.getEntityManager().getItem("prospectus");
+		// Check the description of the item is good.
 		assertThat(newprospectus.getDescription(), is("You see the prospectus that Lon gave to you."));
 
 	}
