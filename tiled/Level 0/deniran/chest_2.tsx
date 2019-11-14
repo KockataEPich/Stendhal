813330c71028d6fa0d31bab7954ad55112f93f40
Bogdan-Gabriel Tudor <bogdan-gabriel.tudor@student.manchester.ac.uk>
2019-11-14 12:53:38 +0000

    Revert "Merge branch 'COMP23311/EX2/flea-market' into 'master'"
    This reverts merge request !3

--- tiled/Level 0/deniran/chest_2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="chest_2" tilewidth="32" tileheight="32" tilecount="6" columns="3">
- <image source="../../tileset/item/chest/chest_2.png" width="96" height="64"/>
-</tileset>
