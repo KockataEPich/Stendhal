a6810654028eafb0beaf6bf269b19f3114b994cf
Benjamin Piggott <benjamin.piggott@student.manchester.ac.uk>
2019-12-04 10:07:41 +0000

    Create JUnit test cases for XML loaders
    Create JUnit tests for the achievement XML loaders, stubs
    of which were created in the previous commit.

---
+++ tests/games/stendhal/server/core/config/AchievementGroupsXMLLoaderTest.java
@@ -0,0 +1,14 @@
+package games.stendhal.server.core.config;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AchievementGroupsXMLLoaderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
