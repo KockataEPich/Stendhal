215915155596ce020e059c342c0fd9551deaeeca
Benjamin Piggott <benjamin.piggott@student.manchester.ac.uk>
2019-12-04 10:01:05 +0000

    Create stubs of XML loaders for testing

---
+++ src/games/stendhal/server/core/config/AchievementXMLLoader.java
@@ -0,0 +1,32 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+package games.stendhal.server.core.config;
+
+import java.net.URI;
+import java.util.Collection;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+
+public final class AchievementXMLLoader extends DefaultHandler {
+
+	public AchievementXMLLoader() {
+		return;
+	}
+
+	public Collection load(URI uri) {
+		return null;
+	}
+}
+
