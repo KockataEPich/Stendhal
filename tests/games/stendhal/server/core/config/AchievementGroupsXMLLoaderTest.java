c9daf2548e15fb21d4814e7d08b73f2455c3a4fd
Benjamin Piggott <benjamin.piggott@student.manchester.ac.uk>
2019-12-05 12:22:18 +0000

    Modify achievement group XML loader test to use DefaultAchievement
    AchievementGroupXMLLoaderTest using DefaultAchievement as
    introduced for use by the loader. Also remove
    AchievementXMLLoaderTest as it is redundant because everything
    it tests AchievementGroupXMLLoaderTest tests also.

--- tests/games/stendhal/server/core/config/AchievementGroupsXMLLoaderTest.java
+++ tests/games/stendhal/server/core/config/AchievementGroupsXMLLoaderTest.java
@@ -7,66 +7,49 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
-import games.stendhal.server.core.engine.StendhalRPZone;
-import games.stendhal.server.core.rp.achievement.*;
-import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.maps.MockStendlRPWorld;
-import marauroa.server.game.db.DatabaseFactory;
-import utilities.PlayerTestHelper;
+//import games.stendhal.server.core.rp.achievement.Category;
+import games.stendhal.server.core.rule.defaultruleset.DefaultAchievement;
+import games.stendhal.server.maps.MockStendlRPWorld;;
 
 /**
  * @author Ben Piggott
  */
 public class AchievementGroupsXMLLoaderTest {
-	private StendhalRPZone zone;
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		MockStendlRPWorld.get();
-		new DatabaseFactory().initializeDatabase();
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		PlayerTestHelper.removeAllPlayers();
-	}
-
 	@Before
 	public void setUp() {
-		zone = new StendhalRPZone("zone", 20, 20);
-		zone.protectionMap.init(1, 1);
-		MockStendlRPWorld.get().addRPZone(zone);
+		MockStendlRPWorld.get();
 	}
 
 	@After
 	public void tearDown() {
-		MockStendlRPWorld.get().removeZone(zone);
+		MockStendlRPWorld.reset();
 	}
 
 	@Test
 	public void testLoad() throws URISyntaxException, SAXException, IOException {
-		final Player player = PlayerTestHelper.createPlayer("dave");
-		zone.add(player);
-		AchievementGroupsXMLLoader loader = new AchievementGroupsXMLLoader(new URI("testachievementgroup.xml"));
-		List<Achievement> list = loader.load();
+		AchievementGroupsXMLLoader loader = new AchievementGroupsXMLLoader(new URI("testachievementgroups.xml"));
+		List<DefaultAchievement> list = loader.load();
 		assertNotNull(list);
 		assertThat(Boolean.valueOf(list.isEmpty()), is(Boolean.FALSE));
-		Achievement achievement = list.get(0);
+		DefaultAchievement achievement = list.get(0);
 		assertThat(achievement.getIdentifier(), is("quest.special.daily.0010"));
-		assertThat(achievement.getCategory(), is(Category.QUEST_ADOS_ITEMS));
+		//assertThat(achievement.getCategory(), is(Category.QUEST_ADOS_ITEMS));
 		assertThat(achievement.getDescription(), is("Finish daily monster quest 10 times"));
 		assertThat(achievement.getTitle(), is("Semos' Protector"));
 		assertThat(achievement.isActive(), is(Boolean.TRUE));
-		assertThat(achievement.getBaseScore(), is(1));
-		assertThat(achievement.isFulfilled(player), is(Boolean.FALSE));
+		//assertThat(achievement.getScore(), is(1));
+		Map<String, String> conditions = achievement.getConditions();
+		assertThat(conditions.get("param1"), is("daily_item"));
+		assertThat(conditions.get("param2"), is(2));
+		assertThat(conditions.get("param3"), is(9));
 	}
 
 }
